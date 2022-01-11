6788c63aec477c4de88583b02b7d874a7e976317
Frus <mcagdasbayirli@gmail.com>
2022-01-11 18:18:23 +0300

    blog04 added

---
+++ pages/blog/blog04.js
@@ -0,0 +1,228 @@
+import React from "react";
+
+const blog04 = () => {
+  return (
+    <section className="lg:py-20 sm:py-16 py-12">
+      <div className="max-w-7xl mx-auto lg:px-8 sm:px-6 px-4">
+        <div className="sm:text-center text-left max-w-lg mx-auto">
+          <h2 className="font-bold sm:text-4xl text-3xl text-slate-800">
+            We release interesting articles about technology
+          </h2>
+        </div>
+        <div className="sm:mt-12 mt-8">
+          <svg
+            className="h-4 w-auto text-gray-400 sm:mx-auto"
+            viewBox="0 0 172 16"
+            fill="none"
+            stroke="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 11 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 46 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 81 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 116 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 151 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 18 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 53 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 88 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 123 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 158 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 25 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 60 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 95 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 130 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 165 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 32 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 67 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 102 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 137 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 172 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 39 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 74 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 109 1)"
+            ></line>
+            <line
+              y1="-0.5"
+              x2="18.0278"
+              y2="-0.5"
+              transform="matrix(-0.5547 0.83205 0.83205 0.5547 144 1)"
+            ></line>
+          </svg>
+        </div>
+        <div className="sm:mt-16 mt-12 max-w-xl mx-auto">
+          <div className="flow-root">
+            <div className="-my-8 divide-y-2">
+              <div className="relative sm:flex sm:items-center py-8 group">
+                <div className="rounded-2xl overflow-hidden flex-shrink-0 w-32 h-32">
+                  <img
+                    src="https://images.unsplash.com/photo-1621057621209-baf1644dc82c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                    alt=""
+                    className="w-full h-full object-cover group-hover:scale-125 transition-all transform duration-300 group-hover:rotate-12"
+                  />
+                </div>
+                <div className="sm:ml-10 sm:mt-0 mt-8">
+                  <p className="font-medium text-slate-800 text-xl">
+                    The unseen of spending three years at Pixelgrade
+                  </p>
+                  <p className="text-sm text-slate-600 mt-5">
+                    January 11, 2022
+                  </p>
+                </div>
+              </div>
+              <div className="relative sm:flex sm:items-center py-8 group">
+                <div className="rounded-2xl overflow-hidden flex-shrink-0 w-32 h-32">
+                  <img
+                    src="https://images.unsplash.com/photo-1616627974469-aeadf7890ed4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1083&q=80"
+                    alt=""
+                    className="w-full h-full object-cover group-hover:scale-125 transition-all transform duration-300 group-hover:rotate-12"
+                  />
+                </div>
+                <div className="sm:ml-10 sm:mt-0 mt-8">
+                  <p className="font-medium text-slate-800 text-xl">
+                    How does writing influence your personal brand?
+                  </p>
+                  <p className="text-sm text-slate-600 mt-5">
+                    January 11, 2022
+                  </p>
+                </div>
+              </div>
+              <div className="relative sm:flex sm:items-center py-8 group">
+                <div className="rounded-2xl overflow-hidden flex-shrink-0 w-32 h-32">
+                  <img
+                    src="https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                    alt=""
+                    className="w-full h-full object-cover group-hover:scale-125 transition-all transform duration-300 group-hover:rotate-12"
+                  />
+                </div>
+                <div className="sm:ml-10 sm:mt-0 mt-8">
+                  <p className="font-medium text-slate-800 text-xl">
+                    Why choose a theme that looks good with WooCommerce
+                  </p>
+                  <p className="text-sm text-slate-600 mt-5">
+                    January 11, 2022
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default blog04;
