22a83e68c5b57050c769a12d7127a708f9acdc53
Frus <mcagdasbayirli@gmail.com>
2022-01-15 17:50:01 +0300

    blog06 added

---
+++ pages/blog/blog06.js
@@ -0,0 +1,173 @@
+import React from "react";
+
+const blog06 = () => {
+  return (
+    <section className="lg:py-20 sm:py-16 py-12 bg-zinc-900">
+      <div className="max-w-7xl mx-auto lg:px-8 sm:px-6 px-4">
+        <div className="max-w-2xl mx-auto text-center">
+          <h2 className="font-bold lg:text-5xl sm:text-4xl text-3xl leading-5 text-white">
+            Stories from blog
+          </h2>
+          <p className="text-gray-300 mt-4">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat
+            fugit repellat, aspernatur reiciendis eos vero.
+          </p>
+        </div>
+        <div className="lg:mt-16 mt-8 lg:max-w-full max-w-md grid lg:grid-cols-3 grid-cols-1 lg:gap-14 gap-7">
+          <div className="shadow-md bg-white rounded-xl overflow-hidden flex flex-col">
+            <div className="px-5 py-6 flex flex-1 flex-col justify-between">
+              <div className="flex-shrink-0">
+                <span className="uppercase font-semibold text-xs tracking-widest text-amber-600">
+                  Lifestyle
+                </span>
+              </div>
+              <div className="flex-1 mt-28">
+                <p className="font-semibold text-2xl">
+                  Power of habit: How to learn anything in 30 days.
+                </p>
+                <p className="mt-4 text-base text-slate-600">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                </p>
+              </div>
+            </div>
+            <div className="border-t">
+              <div className="flex">
+                <div className="px-6 py-5 flex flex-1 items-center">
+                  <img
+                    src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                    alt=""
+                    className="object-cover w-8 h-8 rounded-full"
+                  />
+                  <span className="font-semibold text-base overflow-hidden whitespace-nowrap text-ellipsis ml-3 min-w-0">
+                    Wade Warren
+                  </span>
+                </div>
+                <a
+                  href="/"
+                  className="px-4 py-5 bg-white hover:bg-blue-500 font-semibold transition-colors duration-200 ease-in-out transform inline-flex items-center flex-shrink-0 border-l"
+                >
+                  Read more
+                  <svg
+                    className="w-5 h-5 ml-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-md bg-white rounded-xl overflow-hidden flex flex-col">
+            <div className="px-5 py-6 flex flex-1 flex-col justify-between">
+              <div className="flex-shrink-0">
+                <span className="uppercase font-semibold text-xs tracking-widest text-amber-600">
+                  Technology
+                </span>
+              </div>
+              <div className="flex-1 mt-28">
+                <p className="font-semibold text-2xl">
+                  10 Zoom hacks you can do for productive meetings.
+                </p>
+                <p className="mt-4 text-base text-slate-600">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                </p>
+              </div>
+            </div>
+            <div className="border-t">
+              <div className="flex">
+                <div className="px-6 py-5 flex flex-1 items-center">
+                  <img
+                    src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                    alt=""
+                    className="object-cover w-8 h-8 rounded-full"
+                  />
+                  <span className="font-semibold text-base overflow-hidden whitespace-nowrap text-ellipsis ml-3 min-w-0">
+                    Wade Warren
+                  </span>
+                </div>
+                <a
+                  href="/"
+                  className="px-4 py-5 bg-white hover:bg-blue-500 font-semibold transition-colors duration-200 ease-in-out transform inline-flex items-center flex-shrink-0 border-l"
+                >
+                  Read more
+                  <svg
+                    className="w-5 h-5 ml-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-md bg-white rounded-xl overflow-hidden flex flex-col">
+            <div className="px-5 py-6 flex flex-1 flex-col justify-between">
+              <div className="flex-shrink-0">
+                <span className="uppercase font-semibold text-xs tracking-widest text-amber-600">
+                  Marketing
+                </span>
+              </div>
+              <div className="flex-1 mt-28">
+                <p className="font-semibold text-2xl">
+                  6 Product launching emails you want to use next.
+                </p>
+                <p className="mt-4 text-base text-slate-600">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                </p>
+              </div>
+            </div>
+            <div className="border-t">
+              <div className="flex">
+                <div className="px-6 py-5 flex flex-1 items-center">
+                  <img
+                    src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+                    alt=""
+                    className="object-cover w-8 h-8 rounded-full"
+                  />
+                  <span className="font-semibold text-base overflow-hidden whitespace-nowrap text-ellipsis ml-3 min-w-0">
+                    Jenny Wilson
+                  </span>
+                </div>
+                <a
+                  href="/"
+                  className="px-4 py-5 bg-white hover:bg-blue-500 font-semibold transition-colors duration-200 ease-in-out transform inline-flex items-center flex-shrink-0 border-l"
+                >
+                  Read more
+                  <svg
+                    className="w-5 h-5 ml-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default blog06;
